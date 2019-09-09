cf382dcd0dadbf89cdc30f734db55aacf5977dae
f-osorio <f.osorio@zbw.eu>
2019-09-09 16:58:12 +0200

    Typo
    Last one?

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -18,9 +18,10 @@ def recut():
         temp_dir = find.find_template('..')
     except NonTemplatedInputDirException as e:
         # template coming from Github
-        # make sure `{{cookiecutter.project}}` isn't replaced
+        # Hooks are passed through jinja2. {% raw %} will
+        # Make sure `{{cookiecutter.project}}` isn't replaced
         {% raw %}
-        temp_dir = os.path.abspath(config['cookiecutters_dir'],
+        temp_dir = os.path.join(config['cookiecutters_dir'],
                                 'cookiecutter-ckan-extension',
                                 '{{cookiecutter.project}}')
         {% endraw %}
